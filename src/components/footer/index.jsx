d8fc80137cfc6da1f2055782c61fc81ecac02138
Rafi Rachmawan <rafirachmawan1987@gmail.com>
2024-09-03 13:36:53 +0700

    finished project

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,7 @@
+import "./style.css";
+
+function index() {
+  return <footer>&copy; RumahRafif 2024</footer>;
+}
+
+export default index;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import Navbar from "../components/navbar/index";
 import Header from "../components/header/index";
 import Portofolio from "../components/portofolio/index";
 import About from "../components/about/index";
+import Footer from "../components/footer/index";
+
 function Home() {
   return (
     <>
@@ -9,6 +11,7 @@ function Home() {
       <Header />
       <Portofolio />
       <About />
+      <Footer />
     </>
   );
 }
