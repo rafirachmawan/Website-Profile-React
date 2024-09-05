a87b1994a1bae5c3f6ec52f290ef9681296ce873
Rafi Rachmawan <rafirachmawan1987@gmail.com>
2024-09-06 04:14:01 +0700

    Update Animasi Gsap

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -9,15 +9,48 @@ import {
   FaGithub,
 } from "react-icons/fa";
 import "./style.css";
+import React, { useRef, useEffect } from "react";
+import gsap from "gsap";
 
 function Index() {
+  const headingRef = useRef(null);
+  const descriptionRef = useRef(null);
+
+  useEffect(() => {
+    const animateText = (elementRef) => {
+      const element = elementRef.current;
+      const text = element.innerText;
+      const letters = text
+        .split("")
+        .map((letter, index) =>
+          letter === " " ? "&nbsp;" : `<span class="letter">${letter}</span>`
+        )
+        .join("");
+
+      element.innerHTML = letters;
+
+      gsap.fromTo(
+        element.querySelectorAll(".letter"),
+        { opacity: 0, y: 20 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.5,
+          ease: "power3.out",
+          stagger: 0.05,
+        }
+      );
+    };
+
+    animateText(descriptionRef);
+  }, []);
   return (
     <header>
       <div className="blur-overlay"></div>
       <div className="header-jumbotron">
         <img src={Foto} className="profile-picture" alt="Profile" />
         <h3>Rafi Rachmawan</h3>
-        <p>Programmer - FrontEndDeveloper - Researcher</p>
+        <p ref={descriptionRef}>Programmer - FrontEndDeveloper - Researcher</p>
         <div className="socialMedia">
           <a
             href="https://www.instagram.com/rrrafi.rachmawan"
