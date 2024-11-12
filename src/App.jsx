888ebae372b9436d5281d03a86a93840c8d867da
Rafi Rachmawan <rafirachmawan1987@gmail.com>
2024-11-12 14:08:46 +0700

    Update FontAwesome And Soon Update Mode Dark And Light

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
 import Experience from "./pages/Experience";
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("darkMode") === "true";
+  });
+
+  useEffect(() => {
+    document.body.className = darkMode ? "dark" : "light";
+    localStorage.setItem("darkMode", darkMode);
+  }, [darkMode]);
   return (
     <>
+      <div className="App">
+        <button className="toggle-btn" onClick={() => setDarkMode(!darkMode)}>
+          <FontAwesomeIcon icon={darkMode ? faSun : faMoon} />
+          {darkMode ? " Light Mode" : " Dark Mode"}
+        </button>
+        <h1>Selamat datang di Profil Saya</h1>
+      </div>
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
